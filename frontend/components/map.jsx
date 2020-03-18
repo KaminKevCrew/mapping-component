08555ee44bed77c586a5587b6e015dbdd17ea2e0
KaminKevCrew <durgeisawesome@gmail.com>
2020-03-18 16:21:17 -0700

    Add point of interest to map

--- frontend/components/map.jsx
+++ frontend/components/map.jsx
@@ -76,7 +76,45 @@ class Map extends React.Component {
       "image": "image",
       "video": "video"
     }
-    map.addSource('places', data)
+
+    map.on('load', function () {
+      map.addSource('places', data)
+      map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+          'icon-image': '{icon}-15',
+          'icon-allow-overlap': true
+        }
+      });
+      map.on('click', 'places', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'places', function () {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', 'places', function () {
+        map.getCanvas().style.cursor = '';
+      });
+    });
   }
 
   mapAddRoute(map, coordinates) {
@@ -135,7 +173,7 @@ class Map extends React.Component {
   mapBuilder(map) {
     this.mapAddStyle(map);
     this.mapAddRoute(map, this.coordinates);
-    // this.mapAddPopup(map, this.data);
+    this.mapAddPopup(map, this.data);
   }
 
   componentDidMount() {
